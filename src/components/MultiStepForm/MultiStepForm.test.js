226f2b3c037c0f24ea0b876ce20400aab4bf2be4
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-10-19 16:38:17 +0100

    added multistep tests

--- src/components/MultiStepForm/MultiStepForm.test.js
+++ src/components/MultiStepForm/MultiStepForm.test.js
@@ -1,12 +1,47 @@
-import { MemoryRouter } from "react-router";
-import { MultiStepForm } from "./MultiStepFrom";
-import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MultiStepForm } from "./MultiStepFrom";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
+import { MemoryRouter } from "react-router";
+import * as api from "./form-utils/onSubmit";
 
-describe.skip("component MultiStepFrom", () => {
-  test("render", () => {
+let values = {
+  expired: false,
+  firstName: "john",
+  lastName: "doe",
+  license: "123456",
+  location: "heaven",
+  vehicle: "tesla",
+};
+
+jest.mock("./form-utils/initialValues", () => ({
+  initialValues: values,
+}));
+
+describe("component MultiStepFrom", () => {
+  test("fill the form and sumbmit the values", async () => {
+    const fn = api.onSubmit;
+    api.onSubmit = jest.fn();
+    render(
+      <MemoryRouter initialEntries={["/vehicle"]}>
+        <Provider store={store}>
+          <MultiStepForm />
+        </Provider>
+      </MemoryRouter>
+    );
+    // screen.debug()
+    userEvent.click(await screen.findByText(/next/i));
+
+    await waitFor(() => {
+      expect(api.onSubmit).toHaveBeenCalled();
+      expect(api.onSubmit.mock.calls[0][0]).toEqual(values);
+      // console.log(api.onSubmit.mock.calls[0][0])
+    });
+    api.onSubmit = fn;
+  });
+
+  test("navigation forward", async () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
         <Provider store={store}>
@@ -14,8 +49,63 @@ describe.skip("component MultiStepFrom", () => {
         </Provider>
       </MemoryRouter>
     );
-    const button = screen.getByText(/next/i);
-    userEvent.click(button);
-    // screen.debug();
+    expect(await screen.findByRole("heading", { level: 1 })).toHaveTextContent(
+      /start with your location/i
+    );
+    userEvent.click(screen.getByText(/next/i));
+
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /Please enter your name/i
+      );
+    });
+    userEvent.click(screen.getByText(/next/i));
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /lease enter your license number/i
+      );
+    });
+
+    userEvent.click(screen.getByText(/next/i));
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /Please enter your vehicle/i
+      );
+    });
+  });
+
+  test("navigation backward", async () => {
+    render(
+      <MemoryRouter initialEntries={["/vehicle"]}>
+        <Provider store={store}>
+          <MultiStepForm />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      /Please enter your vehicle/i
+    );
+
+    userEvent.click(screen.getByText(/back/i));
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /lease enter your license number/i
+      );
+    });
+    userEvent.click(screen.getByText(/back/i));
+
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /Please enter your name/i
+      );
+    });
+
+    userEvent.click(screen.getByText(/back/i));
+
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        /start with your location/i
+      );
+    });
   });
 });
